4b590ac32e48013125ccdb07e8f5f90220dc1747
kimanziplacide <p.imanzi@alustudent.com>
2024-02-11 21:07:55 +0200

    starks

--- weird_formatted-dates.py
+++
@@ -1,7 +0,0 @@
-import re
-
-if __name__ == "__main__":
-    test_string = input("Enter your data: ")
-    patterns = r'(\d{2}-[A-Za-z]{3}-\d{4})'
-    matches = re.findall(patterns, test_string)
-    print('Search results: ', matches)
