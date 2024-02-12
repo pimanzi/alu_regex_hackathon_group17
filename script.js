3d7b373a441ea54943fd21973dc32717058e6ca0
Daniel-IRYIVUZE <d.iryivuze@alustudent.com>
2024-02-12 07:02:04 -0800

    Updates

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const inputs = 
-document.querySelectorAll('input'); 
+const inputs = document.querySelectorAll('input'); 
 const patterns = { 
  movies: /^(.+\(\d{4}\))$/, 
  songLyrics: /^(\[Verse\d\]).+$/, 
  twitterNames: /^(@\w+)$/, 
  isbnNumbers: /^(ISBN)(\d{3}-\d-\d{3}-\d{5}-\d)$/,
+
+
+
+ };
+
+function validate(field, regex) {
+   console.log('Field value:', field.value);
+   console.log('Regex test result:', regex.test(field.value));
+
+   if (regex.test(field.value)) {
+       field.classList.remove("invalid");
+       field.classList.add("valid");
+    }
