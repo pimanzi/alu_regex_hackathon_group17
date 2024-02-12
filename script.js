d4fd6bf1c88482ff3ec86aa6d5247e26a71ceaf8
mnelson-1 <m.nelson@alustudent.com>
2024-02-12 10:26:58 -0800

    code added

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const patterns = {
  songLyrics: /^(\[Verse\d\]).+$/, 
  twitterNames: /^(@\w+)$/, 
  isbnNumbers: /^(ISBN)(\d{3}-\d-\d{3}-\d{5}-\d)$/,
-
-
-
+ jokes: /^Why did the .+ \? Because.+$/,
+ tvEpisode: /^(.+)\s(S\d{2})(E\d{2}):.+)$/,
+ dates: /^(\d{2})-(\d{3})-(\d{4})$/,
+ hexColor: /(^#[\D\d]{6}$)/,
  };
 
 function validate(field, regex) {
